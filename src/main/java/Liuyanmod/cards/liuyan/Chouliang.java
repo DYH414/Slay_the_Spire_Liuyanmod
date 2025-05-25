f34b0c77a24d4dd1fe2a83c541503dbf0f326e34
DYH414 <737760221@qq.com>
2025-05-25 17:47:07 +0800

    Revert "refactor(liuyan): 删除未使用的导入语句"
    This reverts commit e9fd3da882a966be664c7e8ccc1ffba0778c3647.

--- src/main/java/Liuyanmod/cards/liuyan/Chouliang.java
+++ src/main/java/Liuyanmod/cards/liuyan/Chouliang.java
@@ -3,9 +3,11 @@ package Liuyanmod.cards.liuyan;
 import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
